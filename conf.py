c7fcc01e7a706021d315a72ae092265f252039c8
Jason Mullings <jason@sonikpass.com>
2020-07-29 16:52:45 -0700

    Changed master file

--- conf.py
+++ conf.py
@@ -12,11 +12,11 @@
 #
 import os
 import sys
-print(os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('..'))
-print(os.path.abspath('../app/management_api/'))
 sys.path.insert(0, os.path.abspath('../app/management_api/'))
 
+
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
