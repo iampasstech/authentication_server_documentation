2a814b2375bfec75bf9bfe7a34d34c491c69a933
Jason Mullings <jason@sonikpass.com>
2020-07-29 17:09:15 -0700

    Small change

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../app/management_api/'))
+sys.path.insert(0, os.path.abspath('../app/authentication_api/'))
 
 
 master_doc = 'index'
